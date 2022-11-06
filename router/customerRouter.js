1c83241b0cca04895e513b5fbb7b9466577cf4ad
Luis Miguel Castro Curequia <luiscastroc11@gmail.com>
2022-11-06 16:28:56 -0500

    Se guardan productos y clientes

--- router/customerRouter.js
+++ router/customerRouter.js
@@ -12,5 +12,6 @@ const customerRoute = express.Router();
 customerRoute.get("/registerCustomer", formRegisterCustomer);
 customerRoute.post("/registerCustomer", createCustomer);
 customerRoute.get("/registerProduct", formRegisterProduct);
+customerRoute.post("/registerProduct", createProduct);
 
 export { customerRoute };
