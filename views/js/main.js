56975497fe6eb3a611b47a333c30524b78ac86e5
Luis Miguel Castro Curequia <luiscastroc11@gmail.com>
2022-11-06 17:37:35 -0500

    se realizan validaciones

--- views/js/main.js
+++ views/js/main.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
+  /* Inicio Menu responsive */
   const menuOpen = document.getElementById("menuOpen"),
     menuClose = document.getElementById("menuClose"),
     navbar = document.querySelector(".navbar");
@@ -11,4 +12,63 @@ window.addEventListener("DOMContentLoaded", () => {
   menuClose.addEventListener("click", () => {
     navbar.classList.remove("navbar--active");
   });
+  /* Fin Menu responsive */
+
+  /* Inicio Validaciones registro clientes */
+  const formCustomer = document.getElementById("formCustomer"),
+    errors = document.getElementById("errors"),
+    inputsForm = document.querySelectorAll(".formRegister__input");
+
+  const expressions = {
+    text: /^[A-Za-z]{0,20}$/i,
+    number: /^[0-9]{0,10}$/i,
+    email: /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/,
+    address: / ^ [a-zA-Z] + [a-zA-Z] + $ /,
+  };
+
+  const validateField = (expression, field, data) => {
+    if (!expression.test(field.value)) {
+      field.classList.add("formRegister__input--error");
+      errors.textContent = `${data} Equivocado`;
+    } else {
+      field.classList.remove("formRegister__input--error");
+      errors.textContent = "";
+    }
+  };
+
+  const validateCustormer = (e) => {
+    switch (e.target.name) {
+      case "name":
+        validateField(expressions.text, e.target, "Nombre");
+        break;
+      case "last_name":
+        validateField(expressions.text, e.target, "Apellido");
+        break;
+      case "document":
+        validateField(expressions.number, e.target, "Documento");
+        break;
+      case "email":
+        validateField(expressions.email, e.target, "Correo");
+        break;
+      case "address":
+        validateField(expressions.address, e.target, "Dirección");
+        break;
+      case "phone":
+        validateField(expressions.number, e.target, "Telefono");
+        break;
+    }
+  };
+
+  inputsForm.forEach((input) => {
+    input.addEventListener("keyup", validateCustormer);
+  });
+
+  formCustomer.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  /* Fin Validaciones registro clientes */
+
+  /* Inicio Validaciones registro productos */
+  /* Fin Validaciones registro productos */
 });
